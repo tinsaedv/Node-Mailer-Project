b172803f58d79bcaff1c9b3bcacea5a0d4aea8c4
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-01-10 13:45:18 +0300

    feat(app): add support for parsing JSON requests
    Added middleware to parse incoming JSON requests in the Express app. This allows the application to handle JSON data sent from clients more effectively.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-
 const express = require('express');
 
 const router = require('./routes/nodemailer.routes');
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/api', router);
 
