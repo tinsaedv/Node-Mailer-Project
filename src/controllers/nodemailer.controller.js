597ad65a64e9d1813e0e02843b234a627a2381a5
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-01-10 12:44:43 +0300

    fix(nodemailer): remove commented out code and unused function call
    Removed commented out code and an unused function call from nodemailer.controller.js to improve code cleanliness and readability.

--- src/controllers/nodemailer.controller.js
+++ src/controllers/nodemailer.controller.js
@@ -5,7 +5,6 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
@@ -40,13 +39,6 @@ async function main(userEmail) {
 
   console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
 }
 
-// main('binukbe4@gmail.com', 'gohapo4862@tanlanav.com');
 module.exports = { main };
