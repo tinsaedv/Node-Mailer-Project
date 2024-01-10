1bdd3ad1a77f2775fc4539777546e3a5ca39e22f
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-01-10 13:45:42 +0300

    feat(server): add asynchronous server start
    - Added an asynchronous function to start the server, which now includes a connection to MongoDB before listening on port 3000. This change ensures that the server only starts once the database connection is established, improving reliability and error handling.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
-
+require('dotenv').config();
 const app = require('./app');
+const mongoConnect = require('./utils/mongo');
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+// Start the server
+async function startServer() {
+  try {
+    await mongoConnect();
+    server.listen(3000, () => {
+      console.log('Server running on port 3000');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+startServer();
