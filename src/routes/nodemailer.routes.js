fc3f123c4cf706fad1e7d709dc26ef2a3d16c98d
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2024-01-24 08:52:12 +0300

    ⚙️chore(routes): Remove nodemailer.routes.js

--- src/routes/nodemailer.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { main, checkOtp } = require('../controllers/nodemailer.controller');
-
-const router = express.Router();
-
-router.post('/send', main).get('/check-otp', checkOtp);
-
-module.exports = router;
